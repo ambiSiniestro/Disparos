fd21e7acaa792354d5472c147e7453d3cd228402
Cristian Castro V <gabb.castro@gmail.com>
2018-01-13 19:45:47 -0300

    Se crea escena para enemigos (template)

---
+++ Scripts/Enemigos.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+export var velocidad = Vector2()
+
+func _ready():
+	set_process(true)
+	pass
+
+func _process(delta):
+	translate(velocidad * delta)
+	if get_pos().y - 16 > get_viewport_rect().size.height:
+		queue_free()
+	pass
